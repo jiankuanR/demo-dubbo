40b7c433f50f5fa57bbb630212ae436a009019fb
luowei <luowei@cloudwalk.cn>
2019-01-04 19:22:08 +0800

    调通服务端

--- demo-dubbo-provider/src/main/java/com/roway/demodubboprovider/DemoDubboProviderApplication.java
+++ demo-dubbo-provider/src/main/java/com/roway/demodubboprovider/DemoDubboProviderApplication.java
@@ -1,9 +1,11 @@
 package com.roway.demodubboprovider;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class DemoDubboProviderApplication {
 
     public static void main(String[] args) {
